d211d2ca96038c1b9b4504df141b3604560bf45e
Gilson Bravo Fernando Chipombo <gbravo-f@c1r2s2.42luanda.com>
2025-05-21 18:43:32 +0100

    Crie uma classe ContaBancaria com atributos saldo e sacar(double valor). Lance uma exceção se o saque for maior que o saldo. Trate a exceção no main()

---
+++ AccountBank.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+ 
+class AccountBank {
+    private: 
+        double saldo;
+    public:
+        AccountBank();
+        double sacar(double amount);
+        double getSaldo();
+        void   setSaldo(double amount);
+};
+
+AccountBank::AccountBank(): saldo(0){}
+
+double AccountBank::sacar(double amount)
+{
+    if (amount > getSaldo())
+        throw "Saldo insuficiente";
+    if (amount < 0)
+        throw "Nao pode sacar quantidade negativa";
+    saldo = saldo - amount;
+    return (saldo);
+}
+
+void AccountBank::setSaldo(double amount)
+{
+    if (amount < 0)
+        throw "Nao pode depositar saldo negativo";
+    saldo = amount;
+}
+
+double AccountBank::getSaldo()
+{
+    return (saldo);
+}
+
+int main()
+{
+    AccountBank c;
+
+    try{
+        c.setSaldo(100);
+        c.sacar(-1);
+        std::cout << "Saldo restante: " << c.getSaldo() << std::endl; 
+    }catch(const char* msg)
+    {
+        std::cout << msg << std::endl;
+    }
+
+}
